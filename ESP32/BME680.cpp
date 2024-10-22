cd4b1adae69899206917cf314bde9b4b668a561c
Colinev86 <92886699+Colinev86@users.noreply.github.com>
2024-10-21 23:59:24 -0400

    Update BME680.cpp

--- ESP32/BME680.cpp
+++ ESP32/BME680.cpp
@@ -1,18 +1,19 @@
-#include "BME280.h"
+#include "BME680.h"
+#define SEALEVELPRESSURE_HPA (1013.25)
 
-//Here is our functions for our BME280 sensor. We'll call these functions to send the data to firebase.
+//Here is our functions for our BME680 sensor. We'll call these functions to send the data to firebase.
 
-Adafruit_BME280 myBME; //We need to create a BME object to store/send data to firebase
+Adafruit_BME680 myBME; //We need to create a BME object to store/send data to firebase
 
 //Starting the BME sensor. If it's not detected, then a message will appear.
 void BME_Start(){
 
-  if(!myBME.begin(0x76)) { //If the BME280 is not detected
+  if(!myBME.begin(0x76)) { //If the BME680 is not detected
     Serial.println("Could not find the BME sensor.");
     while(1); //While true, this loop will run infinitely.
   }
 
-  else if(myBME.begin(0x76)) { //If the BME280 is detected
+  else if(myBME.begin(0x76)) { //If the BME680 is detected
     Serial.println("Connected to the BME sensor!");
     delay(1000); //Wait 1 second afterwards.
   }
@@ -45,6 +46,24 @@ float BMEHumidity() {
  // Serial.println(" %");
   return pressure;
  }
+
+ float BMEGas() {
+  float gas = myBME.gas_resistance() / 1000.0;
+  Serial.print("Gas Resistance = ");
+  Serial.println(gas);
+ // Serial.println(" kOhms");
+  return gas;
+ }
+
+ float BMEAltitude() {
+  float altitude = myBME.readAltitude(SEALEVELPRESSURE_HPA);
+  Serial.print("Altitude = ");
+  Serial.println(altitude);
+ // Serial.println(" m");
+  return altitude;
+ }
+
+
 
 
 
